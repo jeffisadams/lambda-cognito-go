9321eb2386ada12b170ad0cdfaa5f70c7c861229
Jeff Adams <jeffisadams@gmail.com>
2019-06-18 15:37:46 -0400

    Adding more structure and differentiated the error from the success response.

--- src/handler.go
+++ src/handler.go
@@ -10,7 +10,7 @@ import (
 func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "{\"message\":\"Marshalling a return body is a problem for another day.  But the request was successful\"}",
+		Body:       "{\"message\":\"Marshalling a return body is a problem for another day.  But the request was successful\",\"structure\":\"See this is actually not an error\"}",
 		// This is important as part of the CORS config.
 		// Again you should know the security implications of CORS before implementing this
 		Headers: map[string]string{
